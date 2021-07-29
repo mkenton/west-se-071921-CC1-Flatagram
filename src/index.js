ca14e5240dcfef9e3cd7219c7dba0472c07028d4
Michael <mkenton2020@icloud.com>
2021-07-29 12:16:51 -0500

    fully working with deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,68 @@
-// write your code here
+// used defer in script, so all code below will run after DOM Content loads
+
+//declare reused variables
+const baseURL = 'http://localhost:3000';
+const imgURL = 'http://localhost:3000/images/1';
+const commentsURL = 'http://localhost:3000/comments';
+let fetchedDataObject = {};
+let ul = document.querySelector('#fg-comments');
+let form = document.querySelector('#fg-comment-form');
+let likeBttn = document.querySelector('#fg-like-button');
+let span = document.querySelector("#fg-likes");
+let likes;
+form.addEventListener('submit', addNewComment) // add event listener to formfor more comments
+likeBttn.addEventListener('click', incrementLikes); ///add event listener to like button 
+
+// fetch data from server (image, title, likes and comments when page loads
+function getAllData() {
+    fetch(imgURL)
+        .then(resp => resp.json())
+        .then(handleData)
+}
+
+function handleData(data) {
+    fetchedDataObject = data; //create js object with data in case we need it to manipulate later
+    
+    likes = data.likes; //start dog off with correct likes from fetched data
+    //run deliverable functionality
+    addImage(data);
+    addComments(data);
+}
+
+function addImage(data) {
+    //console.log(data);
+    let img = document.querySelector('#fg-image');
+    img.src = data.image;
+}
+
+function addComments(data) {
+    //add initial comments
+    //console.log(data.comments[1].content)
+    ul.innerHTML = `
+    <li> ${data.comments[0].content} </li>
+    <li> ${data.comments[1].content} </li>
+    <li> ${data.comments[2].content} </li>`;
+}
+
+// add new comment when text is typed in form and submitted
+function addNewComment(e) {
+    e.preventDefault();
+    //console.log(e.target.comment.value)
+    const li = document.createElement('li')
+    li.textContent = e.target.comment.value
+    ul.append(li)
+}
+
+//increase likes when like button is cliekd
+function incrementLikes() {
+    likes = likes + 1
+    //console.log(likes)
+    span.textContent = `${likes} likes`
+
+}
+
+function initialize() {
+    getAllData()
+}
+
+initialize()
